692f799004e98309bd299e62b3f3e472f092ec9f
Fábio <fabio.goncalves@school.mindera.com>
2024-02-06 20:16:33 +0000

    feat(exception): create custom exception to be throw in case of null or empty model fields

--- src/main/java/com/mindera/fabio/usersdemo/exceptions/UserFieldsCannotBeNullOrEmptyException.java
+++ src/main/java/com/mindera/fabio/usersdemo/exceptions/UserFieldsCannotBeNullOrEmptyException.java
@@ -1,2 +1,7 @@
-package com.mindera.fabio.usersdemo.exceptions;public class UserFieldsCannotBeNullOrEmptyException {
+package com.mindera.fabio.usersdemo.exceptions;
+
+public class UserFieldsCannotBeNullOrEmptyException extends RuntimeException{
+    public UserFieldsCannotBeNullOrEmptyException() {
+        super("User fields cannot be null or empty");
+    }
 }
