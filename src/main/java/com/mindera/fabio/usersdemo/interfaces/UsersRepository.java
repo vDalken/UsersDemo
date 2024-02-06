d2f2a2e83d488d6132784428981af3bd265c96cb
Fábio <fabio.goncalves@school.mindera.com>
2024-02-06 20:15:40 +0000

    feat(repository): add useful methods for integration and unit testing the repo

--- src/main/java/com/mindera/fabio/usersdemo/interfaces/UsersRepository.java
+++ src/main/java/com/mindera/fabio/usersdemo/interfaces/UsersRepository.java
@@ -2,10 +2,16 @@ package com.mindera.fabio.usersdemo.interfaces;
 
 import com.mindera.fabio.usersdemo.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 //declares a JPA repository that is going to handle users entities
 //a JPA repository should be given to each entity
 @Repository
 public interface UsersRepository extends JpaRepository<User, Long> {
+    @Query(value = "SELECT MAX(id) FROM users", nativeQuery = true)
+    Long findMaxUserId();
+
+    @Query(value = "SELECT COUNT(id) FROM users", nativeQuery = true)
+    Long countAllUsers();
 }
