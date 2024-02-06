29c7cd6bde80cfe04bf17e906480ef2f6bac1df4
Fábio <fabio.goncalves@school.mindera.com>
2024-02-06 20:14:40 +0000

    feat(exceptions): handle all exceptions that might be throw during runtime

--- src/main/java/com/mindera/fabio/usersdemo/exception_handler/ControllerExceptionHandler.java
+++ src/main/java/com/mindera/fabio/usersdemo/exception_handler/ControllerExceptionHandler.java
@@ -1,14 +1,34 @@
 package com.mindera.fabio.usersdemo.exception_handler;
 
+import com.mindera.fabio.usersdemo.exceptions.UserCannotBeNullException;
 import com.mindera.fabio.usersdemo.exceptions.UserDoesNotMatchException;
+import com.mindera.fabio.usersdemo.exceptions.UserFieldsCannotBeNullOrEmptyException;
+import com.mindera.fabio.usersdemo.exceptions.UserNotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
-public class ExceptionHandler {
+public class ControllerExceptionHandler {
 
     @ExceptionHandler(UserDoesNotMatchException.class)
-    public ResponseEntity<String> handleUserDoesNotMatch(UserDoesNotMatchException e){
+    public ResponseEntity<String> handleUserDoesNotMatchException(UserDoesNotMatchException e){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
 
+    @ExceptionHandler(UserCannotBeNullException.class)
+    public ResponseEntity<String> handleUserCannotBeNullException(UserCannotBeNullException e){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+    @ExceptionHandler(UserFieldsCannotBeNullOrEmptyException.class)
+    public ResponseEntity<String> handleUserFieldsCannotBeNullOrEmptyException(UserFieldsCannotBeNullOrEmptyException e){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<String> handleUserNotFoundException(UserNotFoundException e){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
     }
 }
