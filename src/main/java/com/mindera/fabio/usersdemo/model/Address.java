8e99fdd2173aa91159298128598e354b12153a4c
Fábio <fabiogoncalvesk2001@gmail.com>
2024-02-11 17:49:00 +0000

    feat(model): add address model class

--- src/main/java/com/mindera/fabio/usersdemo/model/Address.java
+++ src/main/java/com/mindera/fabio/usersdemo/model/Address.java
@@ -1,2 +1,34 @@
-package com.mindera.fabio.usersdemo.model;public class Address {
+package com.mindera.fabio.usersdemo.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Objects;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "address")
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String country;
+    private String city;
+    private String street;
+    private Integer number;
+
+    @Override
+    public boolean equals(Object address) {
+        if(getClass() != address.getClass()){
+            return false;
+        }
+        Address convertedAddress = (Address) address;
+        return Objects.equals(id, convertedAddress.id) &&
+                Objects.equals(country, convertedAddress.country) &&
+                Objects.equals(city, convertedAddress.city) &&
+                Objects.equals(street, convertedAddress.street) &&
+                Objects.equals(number, convertedAddress.number);
+    }
 }
