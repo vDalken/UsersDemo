9a551299bfaf84140857e198c19dc8429a7b68b8
Fábio <fabiogoncalvesk2001@gmail.com>
2024-02-13 23:52:38 +0000

    refactor(test): add at least one assertion to contextLoads method

--- src/test/java/com/mindera/fabio/usersdemo/UsersdemoApplicationTests.java
+++ src/test/java/com/mindera/fabio/usersdemo/UsersdemoApplicationTests.java
@@ -5,11 +5,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 @SpringBootTest
 class UsersdemoApplicationTests {
+
+	@Autowired
+	private ApplicationContext applicationContext;
+
 	@Test
 	void contextLoads() {
-
+		assertNotNull(applicationContext);
 	}
 
 }
