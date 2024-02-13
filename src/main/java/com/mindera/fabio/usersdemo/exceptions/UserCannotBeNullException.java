6cd16680b5e767ebe8cb2da908f5e63aab032e93
Fábio <fabiogoncalvesk2001@gmail.com>
2024-02-13 14:36:25 +0000

    feat(repository): add custom query method to use in tests

--- src/main/java/com/mindera/fabio/usersdemo/exceptions/UserCannotBeNullException.java
+++
@@ -1,7 +0,0 @@
-package com.mindera.fabio.usersdemo.exceptions;
-
-public class UserCannotBeNullException extends RuntimeException{
-    public UserCannotBeNullException() {
-        super("User object cannot be null");
-    }
-}
--- src/main/java/com/mindera/fabio/usersdemo/interfaces/AddressesRepository.java
+++ src/main/java/com/mindera/fabio/usersdemo/interfaces/AddressesRepository.java
@@ -2,8 +2,11 @@ package com.mindera.fabio.usersdemo.interfaces;
 
 import com.mindera.fabio.usersdemo.model.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AddressesRepository extends JpaRepository<Address, Long> {
+    @Query(value = "SELECT MAX(id) FROM address", nativeQuery = true)
+    Long findMaxAddressId();
 }
