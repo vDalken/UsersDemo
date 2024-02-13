a95732f87bac55ea8502616ea8c3f39365106c7b
Fábio <fabiogoncalvesk2001@gmail.com>
2024-02-13 23:13:22 +0000

    feat(security): add basic endpoint security

--- src/main/java/com/mindera/fabio/usersdemo/security/CustomWebSecurityConfigurerAdapter.java
+++ src/main/java/com/mindera/fabio/usersdemo/security/CustomWebSecurityConfigurerAdapter.java
@@ -1,6 +1,21 @@
 package com.mindera.fabio.usersdemo.security;
 
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
-public class  extends WebSecurityConfigurerAdapter {
+@Configuration
+@EnableWebSecurity
+public class CustomWebSecurityConfigurerAdapter{
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication() //authentication is configured directly within the application's code
+                .withUser("username") //username of the user should be username
+                .password("{noop}password") //password of the user should be a plain string saying password
+                .roles("user"); //categorizing the user based on their permission
+    }
+
 }
