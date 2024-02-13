180d3e3781797e2852df7a4c5586eab455750851
Fábio <fabiogoncalvesk2001@gmail.com>
2024-02-13 19:25:58 +0000

    refactor(test): remove unnecessary tests from application class

--- src/test/java/com/mindera/fabio/usersdemo/UsersdemoApplicationTests.java
+++ src/test/java/com/mindera/fabio/usersdemo/UsersdemoApplicationTests.java
@@ -5,17 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
 @SpringBootTest
 class UsersdemoApplicationTests {
-
-	@Autowired
-	private ApplicationContext applicationContext;
-
 	@Test
 	void contextLoads() {
-		assertNotNull(applicationContext);
+
 	}
 
 }
