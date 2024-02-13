2c83435ce4b99f23311d6072ae434776e92ce6d5
Fábio <fabiogoncalvesk2001@gmail.com>
2024-02-13 23:12:46 +0000

    build(dependencies): add dependency for spring security

---
+++ src/main/java/com/mindera/fabio/usersdemo/security/CustomWebSecurityConfigurerAdapter.java
@@ -0,0 +1,6 @@
+package com.mindera.fabio.usersdemo.security;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+public class  extends WebSecurityConfigurerAdapter {
+}
