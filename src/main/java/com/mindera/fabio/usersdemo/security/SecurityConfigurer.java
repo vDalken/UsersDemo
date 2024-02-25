19db6d9280b12caf1068da142e38bad845cad537
Fábio <fabiogoncalvesk2001@gmail.com>
2024-02-25 19:23:39 +0000

    feat(name): create a component based authentication filter with oatuh2

--- src/main/java/com/mindera/fabio/usersdemo/security/SecurityConfigurer.java
+++ src/main/java/com/mindera/fabio/usersdemo/security/SecurityConfigurer.java
@@ -1,9 +1,11 @@
 package com.mindera.fabio.usersdemo.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -16,6 +18,17 @@ public class SecurityConfigurer {
                 .withUser("username") //username of the user should be username
                 .password("{noop}password") //password of the user should be a plain string saying password
                 .roles("user"); //categorizing the user based on their permission
+    @Bean
+    public SecurityFilterChain authenticationFilterChain(HttpSecurity http) throws Exception{
+        return http
+                .authorizeHttpRequests((authorize) -> {
+                    authorize.requestMatchers("/").permitAll();
+                    authorize.anyRequest().authenticated();
+                })
+                .oauth2Login(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults())
+                .build();
+    }
     }
 
 }
