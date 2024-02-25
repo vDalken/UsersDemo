012b2d1b9238c963be614bfd640aa7d8ba8b0077
Fábio <fabiogoncalvesk2001@gmail.com>
2024-02-25 19:26:54 +0000

    feat(security): Add BCryptPasswordEncoder bean in order to hash passwords

--- src/main/java/com/mindera/fabio/usersdemo/security/SecurityConfigurer.java
+++ src/main/java/com/mindera/fabio/usersdemo/security/SecurityConfigurer.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
@@ -27,6 +28,10 @@ public class SecurityConfigurer {
                 .formLogin(Customizer.withDefaults())
                 .build();
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
